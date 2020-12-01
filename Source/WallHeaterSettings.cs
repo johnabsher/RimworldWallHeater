96d3e03acf8e5d37ec8a9f0383629bd8cfb61d01
John Absher <john.a.absher@gmail.com>
2020-11-30 22:13:39 -0500

    Fixing default heater strength

--- Source/WallHeaterSettings.cs
+++ Source/WallHeaterSettings.cs
@@ -9,29 +9,12 @@ namespace WallHeater
 {
     class WallHeaterSettings : ModSettings
     {
-        internal static float heaterPower;
-        internal static float coolerPower;
+        public static float heaterPower = 21f;
 
         public override void ExposeData()
         {
+            Scribe_Values.Look(ref heaterPower, "heaterPower", 21f);
             base.ExposeData();
-
-            Scribe_Values.Look(ref heaterPower, "heaterPower");
-            Scribe_Values.Look(ref coolerPower, "coolerPower");
-
-            // Remove all null entries in the oreDictionary
-            // This is most likely due to removing a mod, which will trigger a game reset
-            if (Scribe.mode == LoadSaveMode.LoadingVars)
-            {
-                if (heaterPower == 0f)
-                {
-                    heaterPower = 21f;
-                }
-                if (coolerPower == 0f)
-                {
-                    coolerPower = -15f;
-                }
-            }
         }
     }
 }
